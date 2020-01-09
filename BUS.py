041578d5a683a8e2213661a01e18f18c90217815
daawonn <58808398+daawonn@users.noreply.github.com>
2020-01-09 19:57:46 +0900

    Update BUS.py
    f

--- BUS.py
+++ BUS.py
@@ -3,6 +3,6 @@ import os
 import numpy as np 
 import codecs
 
-df = pd.read_csv('./bus.csv',encoding='euc-kr')
+df = pd.read_csv('./bus.csv', encoding='euc-kr')
 print(df.head())
-print(df.isnull().sum())
+print(df.isnull().sum()) 
