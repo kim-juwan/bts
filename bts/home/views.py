56b3b6d03acbff24b3ba53704640a666f5a87c22
kimjannet93 <kimjannet93@naver.com>
2020-01-09 21:21:35 +0900

    j

--- bts/home/views.py
+++ bts/home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def index2(request):
+    return render(request,'index2.html')
