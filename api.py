429b814c19c3129a0b0016312b009662b988ab6e
kim-juwan <juwan92@naver.com>
2020-01-09 11:12:09 +0900

    j

--- api.py
+++ api.py
@@ -3,13 +3,27 @@ from bs4 import BeautifulSoup
 import time, urllib.request, requests, json, csv, random, os
 from selenium.webdriver import ChromeOptions
 import cx_Oracle as oci
+conn = oci.connect('admin/1234@192.168.99.100:32764/xe',encoding='utf-8')
+cursor = conn.cursor()
 
-open_api = 'https://dapi.kakao.com/v2/local/geo/coord2address.json?x=%s&y=%s&input_coord=WGS84'%(129.087635632412,35.251023107627)
-api_key = 'f56b92905ade194d1254314f9e91d103'
+select_sql = 'SELECT (BUSTOPID,X,Y) FROM BUS'
+cursor.execute(select_sql)
+xys = cursor.fetchall()
 
-res = requests.get(open_api, headers={'Authorization' : 'KakaoAK ' + api_key } )
-dic1 = res.json()
+for xy in xys:
+    bustop = xy[0]
+    x = xy[1]
+    y = xy[2]
+    open_api = 'https://dapi.kakao.com/v2/local/geo/coord2address.json?x=%s&y=%s&input_coord=WGS84'%(x,y)
+    api_key = 'f56b92905ade194d1254314f9e91d103'
 
-result = dic1['documents'][0]['address']['address_name']
+    res = requests.get(open_api, headers={'Authorization' : 'KakaoAK ' + api_key } )
+    dic1 = res.json()
 
-print(result)
+    result = dic1['documents'][0]['address']['address_name']
+
+    print(result)
+
+    update_sql = 'UPDATE BUS SET 시군구=:1, 읍면동=:2 WHERE BUSTOPID=:3'
+    cursor.execute(sql,data)
+    conn.commit()
---
+++ test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time, urllib.request, requests, json, csv, random, os
+from selenium.webdriver import ChromeOptions
+import cx_Oracle as oci
+
+open_api = 'https://dapi.kakao.com/v2/local/geo/coord2address.json?x=%s&y=%s&input_coord=WGS84'%(129.069405,35.181774912882)
+api_key = 'f56b92905ade194d1254314f9e91d103'
+
+res = requests.get(open_api, headers={'Authorization' : 'KakaoAK ' + api_key } )
+dic1 = res.json()
+
+result = dic1['documents'][0]['address']['address_name']
+
+print(result)
+
