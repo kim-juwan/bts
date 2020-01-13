530c5ee5277b34707cc258e1545362bae290c6d9
kim-juwan <juwan92@naver.com>
2020-01-14 08:54:03 +0900

    j

--- bts/home/urls.py
+++ bts/home/urls.py
@@ -4,9 +4,7 @@ from . import views   # 현재 패키지에서 views 모듈을 가져옴
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index',views.index,name='index'),
-    path('index2',views.index2,name='index2'),
-    path('index3',views.index3,name='index3'),
-    path('index4',views.index4,name='index4'),
+    path('ppt1',views.ppt1,name='ppt1'),
+    path('ppt2',views.ppt2,name='ppt2'),
 
 ]
--- bts/home/views.py
+++ bts/home/views.py
@@ -2,20 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
-import pandas as pd
-import folium
-import json
-from folium.features import DivIcon
-import cx_Oracle as oci
-import matplotlib.pyplot as plt
 import csv
 
-def home(request):
-    return render(request,'home.html')
 @csrf_exempt
-def index(request):
+def home(request):
     if request.method == "GET":
-        return render(request,'index.html')
+        return render(request,'home.html')
     elif request.method == "POST":
         name = request.POST['name']
         email = request.POST['email']
@@ -23,14 +15,11 @@ def index(request):
         with open('./QnA.csv','a',encoding='euc-kr',newline='') as fp:
             wr = csv.writer(fp, delimiter=',')
             wr.writerow([name,email,message])
-        return render(request,'index.html')
+        return render(request,'home.html')
 
 
-def index2(request):
-    return render(request,'index2.html')
+def ppt1(request):
+    return render(request,'ppt1.html')
 
-def index3(request):
-    return render(request,'index3.html')
-
-def index4(request):
-    return render(request,'index4.html')
+def ppt2(request):
+    return render(request,'ppt2.html')
