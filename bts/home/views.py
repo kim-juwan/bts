e8c9d997172d6ed9cd240a362c3f942353b99ac3
kim-juwan <juwan92@naver.com>
2020-01-13 21:34:05 +0900

    j

--- bts/home/views.py
+++ bts/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 import pandas as pd
 import folium
@@ -7,15 +8,29 @@ import json
 from folium.features import DivIcon
 import cx_Oracle as oci
 import matplotlib.pyplot as plt
+import csv
 
 def home(request):
     return render(request,'home.html')
-
+@csrf_exempt
 def index(request):
-    return render(request,'index.html')
+    if request.method == "GET":
+        return render(request,'index.html')
+    elif request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        with open('./QnA.csv','a',encoding='utf-8') as fp:
+            wr = csv.writer(fp, delimiter=',')
+            wr.writerow([name,email,message])
+        return render(request,'index.html')
+
 
 def index2(request):
     return render(request,'index2.html')
 
 def index3(request):
     return render(request,'index3.html')
+
+def index4(request):
+    return render(request,'index4.html')
